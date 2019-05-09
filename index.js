38377e68e99e571d5c5bcb976a70c4a3d9bbaaa4
seb03000 <avatardu33@gmail.com>
2019-05-09 15:19:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const discord = require('discord.js'),
+client = new discord.Client();
+const token = "votre Token de votre bot";
+const prefix = "!";
+client.login(token);
+
+//Active le bot dans la console CMD comme suite (node index)
+client.on('ready', () =>{
+console.log('Je suis pret :) ');
+});
+
+message.author.send('Actuellement connecté ! : ')
+.then(m =>{
+client.guilds.map(g =>{
+mesage.author.send('\n' + g.name + ''); 
+});
+})
+.catch(err =>{
+console.log(err);
+});
+
+//le bot annonce qui c'est connecté dans le salon général
+client.on('guildMemberAdd', member => {
+const channel = member.guild.channels.find('name', 'general');
+if(!channel) {
+return;
+}
+channel.send(`? **${member.user.username}** Bienvenue sur le serveur **${member.guild.name}**`);
+});
+
+//le bot annonce qui c'est inscrit dans le salon discution
+client.on('guildMemberAdd', member => {
+const channel = member.guild.channels.find('name', 'discution');
+if(!channel) {
+return;
+}
+channel.send(`? **${member.user.username}** Bienvenue sur le serveur **${member.guild.name}**`);
+});
+
+client.on('message', message =>{
+//Moi qui appélle mon bot comme ce si :Bonjour    
+if(message.content.startsWith(prefix + 'Bonjour')){
+//le bot répond   
+message.reply('Bonjour');
+}
+//Moi
+else if (message.content == "sa vas" ){
+//le bot répond   
+message.channel.send('oui et vous ?');
+}
+});
+
+client.on('message', message =>{
+//Moi    
+if(message.content == "oui"){
+//le bot répond   
+message.reply('tant mieux !!!');
+}
+//Moi
+else if (message.content == "tu fait quoi ?" ){
+//le bot répond  
+message.channel.send('je surveille le serveur');
+}
+});
+
+client.on('message', message =>{
+//Moi    
+if(message.content == "cool"){
+//le bot répond   
+message.reply('XD');
+}
+//Moi
+else if (message.content == "MDR" ){
+//le bot répond   
+message.channel.send('LOL');
+} 
+});
